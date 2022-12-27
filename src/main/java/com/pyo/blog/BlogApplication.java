04c70db8e8d10177f96bf37fb87c1749369ecc22
Sunghyun Pyo <vytjdgus1234@naver.com>
2022-12-27 22:49:53 +0900

    Initial commit

---
+++ src/main/java/com/pyo/blog/BlogApplication.java
@@ -0,0 +1,19 @@
+package com.pyo.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+
+		System.out.println("헬로우 월드");
+		System.out.println("헬로우 월드");
+		System.out.println("헬로우 월드");
+		System.out.println("헬로우 월드");
+		System.out.println("헬로우 월드");
+	}
+
+}
