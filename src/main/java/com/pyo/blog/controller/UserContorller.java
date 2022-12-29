a27634893f6a8e0449aa900ace21520b49c9670a
Sunghyun Pyo <vytjdgus1234@naver.com>
2022-12-29 17:28:58 +0900

    인덱스, 회원가입, 로그인 폼 생성

--- src/main/java/com/pyo/blog/controller/UserContorller.java
+++ src/main/java/com/pyo/blog/controller/UserContorller.java
@@ -1,4 +1,20 @@
 package com.pyo.blog.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserContorller {
+
+    @GetMapping("/user/loginForm")
+    public String loginForm(){
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/joinForm")
+    public String joinForm() {
+
+        return "user/joinForm";
+    }
 }
